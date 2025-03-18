81b0dc2a937814065846df1850598bd207defabd
rrollier1 <rrollier1@myges.fr>
2025-03-18 16:03:07 +0100

    15 jours delete

--- deleteoldfile.ps1
+++ deleteoldfile.ps1
@@ -1,2 +1,3 @@
-$Path = Read-Host "Saisissez le chemin vers le dossier a baleyer"
-#test
+$Path = Read-Host "Saisissez le chemin vers le dossier a purger!"
+
+Get-ChildItem -Path $path -Recurse -Force | Where-Object{ !$_.PSIsContainer -and $_. CreationTime -lt (Get-Date).AddDays(-15) } | Remove-Item -Force
